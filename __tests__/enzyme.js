5c8a5a316c19dac0abf5c7f3d2fe5442c5b28b0d
dindean <a123deandean@gmail.com>
2020-10-12 09:47:51 -0400

    change some tests

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import {
-  configure, shallow, mount, render,
-} from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
 import renderer from 'react-test-renderer';
-
-// Enzyme is a wrapper around React test utilities which makes it easier to
-// shallow render and traverse the shallow rendered tree.
 
 import { addCard } from '../client/actions/actions';
 
